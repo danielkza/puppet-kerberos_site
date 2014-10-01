2a6684ff14b06333b170af3ca6ed5e6e120784f4
Daniel Miranda <danielkza2@gmail.com>
2014-10-01 18:44:49 -0300

    Continue work on keytab generation

--- lib/puppet/parser/functions/krb5_generate_keytab.rb
+++ lib/puppet/parser/functions/krb5_generate_keytab.rb
@@ -1,125 +1,120 @@
 require 'digest'
 require 'puppet/util/krb5'
 
-module Puppet::Parser::Functions
-  newfunction(:krb5_generate_keytab, :type => :rvalue, :arity => 1, :doc => %q{
-Generate a keytab for the given princs, and return a puppet file server URL
-that can be used to send it to a client.
-
-This function takes a list of princs as it's only argument: they will be
-included in the generated keytab. If any of them does not have an explicit realm,
-the default one from the 'krb5_default_realm' option in puppet.conf will be used.
+module Puppet::Parser::Functions.newfunction(:krb5_generate_keytab,
+  :type => :rvalue, :arity => 1, :doc => %q{
+Generate a keytab for the given principals, and return the content.
 }) \
-  do |args|
-    # Validate principal names
-    princ_names = args[0]
-    if (! princ_names.is_a?(Enumerable) ||
-        princ_names.any? { |p| ! p.is_a?(String) })
-      raise Puppet::ParseError,
-        "Invalid parameter: must be a non-empty sequence of strings"
-    end
-
-    # Puppet should create the keytab directory for us: bail without trying
-    # to create it if it does not exist
-    keytab_dir = Puppet.settings[:krb5_keytab_dir]
-    if ! keytab_dir || ! File.directory?(keytab_dir)
-      raise Puppet::Error,
-        "Unconfigured or missing keytab dir - '#{keytab_dir}'"
-    end
-
-    mount_point = Puppet.settings[:krb5_keytab_mount_point]
-    if ! mount_point || mount_point.empty?
-      raise Puppet::Error,
-        "File server mount point for keytabs not set"
-    end
-
-    # Retrieve the node's certificate name from the trusted node data 
-    trusted_data = lookupvar('trusted')
-    certname = trusted_data[:certname]
-    raise Puppet::Error, "Failed to retrieve trusted certname" unless certname
-
-    # Create the subdirectories of the keytab dir as needed
-    keytab_dirs = [:principals, certname].reduce({}) do { |h, t|
-      path = File.join(keytab_dir, t.to_s)
-      File.mkdir(path, :mode => 0700) unless File.directory?(path)
-      h[t] = path
-      h
-    }
-
-    # Lookup all princs at once to fail early if one is missing.
-    # Sorted order is needed so the generated hash of the combination of the
-    # principal names is predictable
-    kadmin = Puppet::Util::Krb5.kadmin_instance()
-    princs = princ_names.map { |name|
-      kadmin.get_principal(name) or raise Puppet::Error,
-        "Principal #{n} not found in Kerberos database"
-    }.sort_by(&:name)
-
-    princs_max_mtime = princs.map(&:last_modified_time).max
-
-    # Hash the combination of the principal names and use it as the lookup key
-    # for the combined keytab
-    md5 = Digest::MD5.new
-    princs.each { |p| md5 << p.name }
-    combined_id = Digest.hexencode(md5.digest)
-
-    # Generate the path to a keytab and a temporary file to use for keytab
-    # generation
-    def keytab_paths(type, name)
-      keytab = File.join(keytab_dirs[type], name)
-      temp_keytab = keytab + ':' + DateTime.now().iso8601 + '.keytab'
-      keytab << '.keytab'
-      [keytab, temp_keytab]
-    end
-
-    # Check if a keytab needs to be regenerated, either because it is missing
-    # or because it is outdated
-    def keytab_needs_regen?(path, name = nil, mtime)
-      if ! File.exists?(path) || File.mtime(path) < mtime
-        status = keytab_mtime ? 'outdated' : 'missing'
-        function_info("Keytab for #{name} is #{status}, regenerating") if name
-        true
-      else
-        false
-      end
-    end
-
-    # Replace a keytab with a different file, applying correct permissions and
-    # file times
-    def keytab_replace(src, dest, mtime)
-      FileUtils.mv(src, dest)
-      File.chmod(0600, dest)
-      File.utime(File.atime(dest), mtime)
-    end
-
-    # Check if the combined keytab is up to date first: if it is, we don't
-    # need to do anything else
-    combined_keytab, combined_tmp_keytab = 
-      keytab_paths(certname, combined_id)
-
-    if keytab_needs_regen(combined_keytab, nil, princs_max_mtime)
-      # Check each principal individually and update the keytabs accordingly
-      princ_keytabs = princs.each do |princ|
-        princ_keytab, princ_tmp_keytab =
-          keytab_paths(:principals, princ.name)
-        mtime = princ.last_modified_time
-
-        if keytab_needs_regen(princ_keytab, princ.name, mtime)
-          kadmin.keytab_add(princ.name, false, princ_tmp_keytab)
-          keytab_replace(princ_tmp_keytab, princ_keytab, mtime)
-        end
-
-        princ_keytab
-      end
-
-      # Generated the combined keytab from all the principal keytabs
-      ktutil = Puppet::Util::Krb5::KtUtil.new(combined_tmp_keytab)
-      princ_keytabs.each(&ktutil.method(:add_keytab))
-      ktutil.generate()
-
-      keytab_replace(combined_tmp_keytab, combined_keytab, princs_max_mtime)
-    end
-
-    "puppet://#{mount_point}/#{File.basename(combined_keytab)}"
+do |args|
+  # Validate principal names
+  princ_names = args[0]
+  if (! princ_names.is_a?(Enumerable) ||
+      princ_names.any? { |p| ! p.is_a?(String) }) 
+    raise Puppet::ParseError,
+      'Invalid parameter: must be a non-empty array of strings'
   end
+
+  princ_names.each do |name|
+    begin
+      Puppet::Util::Krb5.principal_check_name(name)
+    rescue ArgumentError => e
+      raise Puppet::ParseError, e.message
+    end
+  end
+
+  # Puppet should create the keytab directory for us: bail without trying
+  # to create it if it does not exist
+  keytab_dir = Puppet.settings[:krb5_keytab_dir]
+  if ! keytab_dir || ! File.directory?(keytab_dir)
+    raise Puppet::Error,
+      "Unconfigured or missing keytab dir - '#{keytab_dir}'"
+  end
+
+  # Retrieve the node's certificate name from the trusted node data 
+  trusted_data = lookupvar('trusted')
+  certname = trusted_data[:certname]
+  raise Puppet::Error, 'Failed to retrieve trusted certname' unless certname
+
+  # Create the subdirectories of the keytab dir as needed
+  keytab_dirs = {:principals => 'principals', :host => certname}
+  keytab_dirs.each do |k, v|
+    v.replace(File.join(keytab_dir, v))
+    File.mkdir(v, :mode => 0700) unless File.directory?(v)
+  }
+
+  # Lookup all principals at once to fail early if one is missing.
+  # Sorted order is needed so the generated hash of the combination of the
+  # principal names is predictable
+  kadmin = Puppet::Util::Krb5.kadmin_instance()
+  princs = princ_names.map { |name|
+    kadmin.get_principal(name) or raise Puppet::Error,
+      "Principal #{n} not found in Kerberos database"
+  }.sort_by(&:name)
+
+  princs_max_mtime = princs.map(&:last_modified_time).max
+
+  # Hash the combination of the principal names and use it as the lookup key
+  # for the combined keytab
+  md5 = Digest::MD5.new
+  princs.each { |p| md5 << p.name }
+  combined_id = Digest.hexencode(md5.digest)
+
+  # Generate the path to a keytab and a temporary file to use for keytab
+  # generation
+  def keytab_paths(type, name)
+    keytab = File.join(keytab_dirs[type], name)
+    temp_keytab = keytab + ':' + DateTime.now().iso8601 + '.keytab'
+    keytab << '.keytab'
+    [keytab, temp_keytab]
+  end
+
+  # Check if a keytab needs to be regenerated, either because it is missing
+  # or because it is outdated
+  def keytab_needs_regen?(path, name = nil, mtime)
+    if ! File.exists?(path) || File.mtime(path) < mtime
+      status = keytab_mtime ? 'outdated' : 'missing'
+      Puppet.info("Keytab for #{name} is #{status}, regenerating") if name
+      true
+    else
+      false
+    end
+  end
+
+  # Replace a keytab with a different file, applying correct permissions and
+  # file times
+  def keytab_replace(src, dest, mtime)
+    File.rename(src, dest)
+    File.chmod(0600, dest)
+    File.utime(File.atime(dest), mtime)
+  end
+
+  # Check if the combined keytab is up to date first: if it is, we don't
+  # need to do anything else
+  combined_keytab, combined_tmp_keytab = 
+    keytab_paths(certname, combined_id)
+
+  if keytab_needs_regen(combined_keytab, nil, princs_max_mtime)
+    # Check each principal individually and update the keytabs accordingly
+    princ_keytabs = princs.map do |princ|
+      princ_keytab, princ_tmp_keytab =
+        keytab_paths(:principals, princ.name)
+      mtime = princ.last_modified_time
+
+      if keytab_needs_regen(princ_keytab, princ.name, mtime)
+        kadmin.keytab_add(princ.name, false, princ_tmp_keytab)
+        keytab_replace(princ_tmp_keytab, princ_keytab, mtime)
+      end
+
+      princ_keytab
+    end
+
+    # Generated the combined keytab from all the principal keytabs
+    ktutil = Puppet::Util::Krb5::Ktutil.new(combined_tmp_keytab)
+    princ_keytabs.each(&ktutil.method(:add_keytab))
+    ktutil.write()
+
+    keytab_replace(combined_tmp_keytab, combined_keytab, princs_max_mtime)
+  end
+
+  IO.binread(combined_keytab)
 end
--- lib/puppet/util/krb5/ktutil.rb
+++ lib/puppet/util/krb5/ktutil.rb
@@ -1,59 +1,108 @@
-require 'io/wait'
+require 'open3'
 
-module Krb5
-  class Ktutil
-    def initialize(path, ktutil_bin = nil)
-      if ! path || ! path.is_a?(String) || path.empty?
-        raise ArgumentError, "Invalid path"
-      end
-
-      @dest = path
-      @bin = ktutil_bin || 'ktutil'
-      @keytabs = []
+module Puppet::Util::Krb5
+class Ktutil
+  def initialize(path, ktutil_bin = nil)
+    if ! path || ! path.is_a?(String) || path.empty?
+      raise ArgumentError, "Invalid path"
     end
 
-    def add_keytab(path)
-      keytabs << path
+    @dest = path
+    @bin = ktutil_bin || 'ktutil'
+    @keytabs = []
+  end
+
+  def add_keytab(path)
+    @keytabs << path
+    self
+  end
+
+  def <<(path)
+    add_keytab(path)
+  end
+
+  def write
+    if @keytabs.empty?
+      raise ArgumentError, 'No keytabs added, nothing to generate'
     end
 
-    def generate
-      if @keytabs.empty?
-        raise ArgumentError, "No keytabs added, nothing to generate"
-      end
+    commands = @keytabs.map { |kt| "read_kt #{kt}" }
+    commands << "write_kt #{@dest}"
 
-      env = {'LANG' => 'C', 'LC_ALL' => 'C'}
+    command_done = ready_for_command = false
+    out_done = err_done = false
+    out_text = ''
+    err_text = ''
+    commands_enum = commands.each
 
-      commands = @keytabs.map { |kt| "read_kt #{kt}" }
-      commands << "write_kt #{@dest}" << "quit" 
+    # Interact with the ktutil command. Since our only way to know whether
+    # each command succeeded or not is looking at stderr, but we can't block
+    # by ignoring stdout, we must do something a little more complicated.
+    #
+    # We always read from both stdout and stderr when possible, accumulating
+    # their contents until we get the prompt indicator.
+    #
+    # If there was an error, we bail out. Otherwise, we know we can send
+    # a new command. If we haven't sent all the commands, we send one,
+    # then go back to reading until the command completes. Otherwise, we
+    # send a quit command, and simply wait until ktutil closes both streams,
+    # at which point we'll bail out.
 
-      Open3.Popen3(env, [@bin]) do |in_f, out_f, err_f, thread|
-        out_f.wait_readable()
-        out = read_available(out_f)
+    env = {'LANG' => 'C', 'LC_ALL' => 'C'}
+    in_f, out_f, err_f, wait_thr = Open3.popen3(env, @bin)
 
-        err_f.wait_readable(1)
-        
+    while true
+      read_set = [!out_done ? out_f : nil, !err_done ? err_f : nil].compact
+      break if read_set.empty?
+      read_set = IO.select(read_set)[0]
 
-        if err_f.ready?
-          err = read_available(err)
-
-
-        out_f.wait_readable()
-        @keytabs.each do |kt|
-
-          out.puts()
-
-    private
-
-    def read_available(io)
-      res = ""
-      begin
-        while true
-          res += io.read_nonblock(64)
+      if read_set.include?(out_f)
+        begin
+          out_text += out_f.read_nonblock(512)
+        rescue EOFError
+          out_done = true
+        else
+          command_done = true if out_text =~ /:\s+$/
         end
-      rescue IO::WaitReadable
       end
 
-      res
+      if read_set.include?(err_f)
+        ready_for_command = false
+
+        begin
+          err_text += err_f.read_nonblock(512)
+        rescue EOFError
+          err_done = true
+        end
+      end
+
+      if command_done
+        break if ! err_text.empty?
+        ready_for_command = true
+      end
+
+      if ready_for_command
+        begin
+          command = commands_enum.next
+        rescue StopIteration
+          command = 'quit'
+        end
+
+        command_done = false
+        ready_for_command = false
+        in_f.puts(command)
+      end
     end
 
+    [in_f, out_f, err_f].each(&:close)
+    status = wait_thr.value
+    errors = err_text.split("\n")
+
+    if !errors.empty? || status.exitstatus != 0
+      msg = errors.first || '(no message)'
+      raise KerberosError.from_command('ktutil', msg, status.exitstatus)
+    end
+  end
+end
+end
 
