3331f7a2cffcd15216b756ee36c87dfc1fe94aed
Daniel Miranda <danielkza2@gmail.com>
2014-10-04 16:23:21 -0300

    ktutil: cleanup
    - Check for keytab readibility on addition
    - Improve safety of process handling
    - Improve comments
    - Capture stdout as well as stderr
    - Use array version of popen3 command to avoid going through the shell

--- lib/puppet/util/krb5/ktutil.rb
+++ lib/puppet/util/krb5/ktutil.rb
@@ -2,9 +2,11 @@ require 'open3'
 
 module Puppet::Util::Krb5
 class Ktutil
+  @@exec_env = {'LANG' => 'C', 'LC_ALL' => 'C'}
+  
   def initialize(path, ktutil_bin = nil)
-    if ! path || ! path.is_a?(String) || path.empty?
-      raise ArgumentError, "Invalid path"
+    unless path && path.is_a?(String) && ! path.empty?
+      raise ArgumentError, 'Invalid destination path'
     end
 
     @dest = path
@@ -13,7 +15,11 @@ class Ktutil
   end
 
   def add_keytab(path)
-    @keytabs << path
+    unless File.readable?(path)
+      raise IOError, "Keytab does not exist or is not readable - '#{path}'"
+    end
+
+    @keytabs << File.absolute_path(path)
     self
   end
 
@@ -23,85 +29,97 @@ class Ktutil
 
   def write
     if @keytabs.empty?
-      raise ArgumentError, 'No keytabs added, nothing to generate'
+      raise RuntimeError, 'No keytabs added, nothing to generate'
     end
 
     commands = @keytabs.map { |kt| "read_kt #{kt}" }
     commands << "write_kt #{@dest}"
 
-    command_done = ready_for_command = false
-    out_done = err_done = false
-    out_text = ''
-    err_text = ''
-    commands_enum = commands.each
+    # We need to use the array form of command: if we pass a single string it
+    # will go through shell expansion, which we do not want
+    cmd = [@bin, File.basename(@bin)]
+    out_text = String.new
+    error_text = String.new
 
-    # Interact with the ktutil command. Since our only way to know whether
-    # each command succeeded or not is looking at stderr, but we can't block
-    # by ignoring stdout, we must do something a little more complicated.
-    #
-    # We always read from both stdout and stderr when possible, accumulating
-    # their contents until we get the prompt indicator.
-    #
-    # If there was an error, we bail out. Otherwise, we know we can send
-    # a new command. If we haven't sent all the commands, we send one,
-    # then go back to reading until the command completes. Otherwise, we
-    # send a quit command, and simply wait until ktutil closes both streams,
-    # at which point we'll bail out.
+    process = Open3.popen3(@@exec_env, cmd) do |in_f, out_f, err_f, thread|     
+      command_done = ready_for_command = false
+      out_done = err_done = false
+      commands_enum = commands.each
+      out_buf = String.new
+      err_buf = String.new
 
-    env = {'LANG' => 'C', 'LC_ALL' => 'C'}
-    in_f, out_f, err_f, wait_thr = Open3.popen3(env, @bin)
+      # Interact with the ktutil command. Since our only way to know whether
+      # each command succeeded or not is looking at stderr, but we can't block
+      # by ignoring stdout, we must do something a little more complicated.
+      loop do
+        # Wait for either stdout or stderr to be ready for reading. If they both
+        # have been closed already, break out early.
+        read_set = [!out_done ? out_f : nil, !err_done ? err_f : nil].compact
+        break if read_set.empty?
+        read_set = IO.select(read_set)[0]
 
-    while true
-      read_set = [!out_done ? out_f : nil, !err_done ? err_f : nil].compact
-      break if read_set.empty?
-      read_set = IO.select(read_set)[0]
+        # Read from stdout and accumulate it's contents. Then check if we received
+        # a prompt: if positive, we know we can know send a new command if
+        # everything went okay, or that we have an error message otherwise.
+        if read_set.include?(out_f)
+          begin
+            out_f.read_nonblock(512, out_buf)
+          rescue EOFError
+            out_done = true
+          else
+            command_done = true if out_buf =~ /:\s+$/
+            out_text << out_buf
+          end
+        end
 
-      if read_set.include?(out_f)
-        begin
-          out_text += out_f.read_nonblock(512)
-        rescue EOFError
-          out_done = true
-        else
-          command_done = true if out_text =~ /:\s+$/
+        # Read from stderr: if we do have something, it means an error was thrown,
+        # and that we should refrain from sending any more commands.
+        # Do not bail straight away, but keep running so we can get the complete
+        # message: we know it is done when we get a prompt indicator in stdout,
+        # or when the outputs are closed.
+        if read_set.include?(err_f)
+          ready_for_command = false
+
+          begin
+            err_f.read_nonblock(512, err_buf)
+          rescue EOFError
+            err_done = true
+          else
+            error_text << err_buf
+          end
+        end
+
+        # We received a prompt indicator: we should now break with an error
+        # message, or send out the next command
+        if command_done
+          break unless error_text.empty?
+          ready_for_command = true
+        end
+
+        if ready_for_command
+          begin
+            command = commands_enum.next
+          rescue StopIteration
+            command = 'quit'
+          end
+
+          command_done = false
+          ready_for_command = false
+          in_f.puts(command)
         end
       end
 
-      if read_set.include?(err_f)
-        ready_for_command = false
-
-        begin
-          err_text += err_f.read_nonblock(512)
-        rescue EOFError
-          err_done = true
-        end
-      end
-
-      if command_done
-        break if ! err_text.empty?
-        ready_for_command = true
-      end
-
-      if ready_for_command
-        begin
-          command = commands_enum.next
-        rescue StopIteration
-          command = 'quit'
-        end
-
-        command_done = false
-        ready_for_command = false
-        in_f.puts(command)
-      end
+      thread.value
     end
 
-    [in_f, out_f, err_f].each(&:close)
-    status = wait_thr.value
-    errors = err_text.split("\n")
+    unless process.exitstatus == 0 && error_text.empty? 
+      error_msg = error_text.each_line.first
+      error_msg.chomp! if error_msg
 
-    if !errors.empty? || status.exitstatus != 0
-      msg = errors.first || '(no message)'
-      raise KerberosError.from_command('ktutil', msg, status.exitstatus)
+      raise KerberosError.from_command('ktutil', error_msg, process.exitstatus)
     end
+
+    @keytabs.length
   end
 end
 end
