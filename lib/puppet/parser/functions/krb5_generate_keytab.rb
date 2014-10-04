e9d32883aa1ad6cac5831bedfc604cb505c2913c
Daniel Miranda <danielkza2@gmail.com>
2014-10-04 16:26:36 -0300

    krb5_generate_keytab: improved error resillience all round, cleanups

--- lib/puppet/parser/functions/krb5_generate_keytab.rb
+++ lib/puppet/parser/functions/krb5_generate_keytab.rb
@@ -1,120 +1,171 @@
 require 'digest'
 require 'puppet/util/krb5'
 
-module Puppet::Parser::Functions.newfunction(:krb5_generate_keytab,
+Puppet::Parser::Functions.newfunction(:krb5_generate_keytab,
   :type => :rvalue, :arity => 1, :doc => %q{
 Generate a keytab for the given principals, and return the content.
 }) \
 do |args|
-  # Validate principal names
-  princ_names = args[0]
-  if (! princ_names.is_a?(Enumerable) ||
-      princ_names.any? { |p| ! p.is_a?(String) }) 
+  # Validate parameter
+  if args[0].is_a?(String)
+    princ_names = [args[0]]
+  elsif args[0].is_a?(Array)
+    if args[0].empty?
+      raise Puppet::ParseError, 'Principal list must not be empty'
+    end
+
+    princ_names = args[0]
+  else
     raise Puppet::ParseError,
-      'Invalid parameter: must be a non-empty array of strings'
+      'Invalid parameter: must be a string or list of strings'
   end
 
   princ_names.each do |name|
     begin
       Puppet::Util::Krb5.principal_check_name(name)
     rescue ArgumentError => e
-      raise Puppet::ParseError, e.message
+      raise Puppet::ParseError, e.message, e.backtrace
     end
   end
 
   # Puppet should create the keytab directory for us: bail without trying
   # to create it if it does not exist
   keytab_dir = Puppet.settings[:krb5_keytab_dir]
-  if ! keytab_dir || ! File.directory?(keytab_dir)
-    raise Puppet::Error,
+  unless keytab_dir && Puppet::FileSystem.directory?(keytab_dir)
+    raise Puppet::ParseError,
       "Unconfigured or missing keytab dir - '#{keytab_dir}'"
   end
 
   # Retrieve the node's certificate name from the trusted node data 
   trusted_data = lookupvar('trusted')
-  certname = trusted_data[:certname]
-  raise Puppet::Error, 'Failed to retrieve trusted certname' unless certname
+  certname = trusted_data['certname']
+  unless certname
+    raise Puppet::ParseError,
+      'Failed to retrieve certname from trusted node data'
+  end
+
+  def keytab_dir_maybe_create(path)
+    begin
+      Dir.mkdir(path, 0700) unless Puppet::FileSystem.directory?(path)
+    rescue SystemCallError => e
+      raise Puppet::ParseError, "Failed to create keytab subdirectory: #{e}",
+        e.backtrace
+    end
+  end
 
   # Create the subdirectories of the keytab dir as needed
-  keytab_dirs = {:principals => 'principals', :host => certname}
-  keytab_dirs.each do |k, v|
-    v.replace(File.join(keytab_dir, v))
-    File.mkdir(v, :mode => 0700) unless File.directory?(v)
-  }
+  keytab_dirs = {:principals => 'principals', :host => certname}.tap do |h|
+    h.each do |k, v|
+      h[k] = v = File.join(keytab_dir, v)
+      keytab_dir_maybe_create(v)
+    end
+  end
 
   # Lookup all principals at once to fail early if one is missing.
   # Sorted order is needed so the generated hash of the combination of the
   # principal names is predictable
-  kadmin = Puppet::Util::Krb5.kadmin_instance()
-  princs = princ_names.map { |name|
-    kadmin.get_principal(name) or raise Puppet::Error,
-      "Principal #{n} not found in Kerberos database"
-  }.sort_by(&:name)
-
-  princs_max_mtime = princs.map(&:last_modified_time).max
-
-  # Hash the combination of the principal names and use it as the lookup key
-  # for the combined keytab
-  md5 = Digest::MD5.new
-  princs.each { |p| md5 << p.name }
-  combined_id = Digest.hexencode(md5.digest)
-
-  # Generate the path to a keytab and a temporary file to use for keytab
-  # generation
-  def keytab_paths(type, name)
-    keytab = File.join(keytab_dirs[type], name)
-    temp_keytab = keytab + ':' + DateTime.now().iso8601 + '.keytab'
-    keytab << '.keytab'
-    [keytab, temp_keytab]
+  begin
+    kadmin = Puppet::Util::Krb5.kadmin_instance
+    princs = princ_names.map { |name|
+      kadmin.get_principal(name) or raise Puppet::ParseError,
+        "Principal #{n} not found in Kerberos database"
+    }.sort_by(&:name)
+  rescue Pupppet::Util::Krb5::KerberosError => e
+    raise Puppet::ParseError,
+      "Failed to retrieve Kerberos principal information: #{e}", e.backtrace
   end
 
-  # Check if a keytab needs to be regenerated, either because it is missing
-  # or because it is outdated
-  def keytab_needs_regen?(path, name = nil, mtime)
-    if ! File.exists?(path) || File.mtime(path) < mtime
-      status = keytab_mtime ? 'outdated' : 'missing'
-      Puppet.info("Keytab for #{name} is #{status}, regenerating") if name
-      true
-    else
-      false
+  # Hash the combination of the principal names and versions, and use it as the
+  # lookup key for the combined keytab
+  combined_id = Digest::MD5.new.tap { |m|
+    princs.each do |p|
+      m << "#{p.name}|#{p.master_key_version}|#{p.key_version}|"
     end
-  end
+  }.hexdigest
 
-  # Replace a keytab with a different file, applying correct permissions and
-  # file times
-  def keytab_replace(src, dest, mtime)
-    File.rename(src, dest)
-    File.chmod(0600, dest)
-    File.utime(File.atime(dest), mtime)
-  end
-
-  # Check if the combined keytab is up to date first: if it is, we don't
-  # need to do anything else
-  combined_keytab, combined_tmp_keytab = 
-    keytab_paths(certname, combined_id)
-
-  if keytab_needs_regen(combined_keytab, nil, princs_max_mtime)
-    # Check each principal individually and update the keytabs accordingly
-    princ_keytabs = princs.map do |princ|
-      princ_keytab, princ_tmp_keytab =
-        keytab_paths(:principals, princ.name)
-      mtime = princ.last_modified_time
-
-      if keytab_needs_regen(princ_keytab, princ.name, mtime)
-        kadmin.keytab_add(princ.name, false, princ_tmp_keytab)
-        keytab_replace(princ_tmp_keytab, princ_keytab, mtime)
+  # Regenerate a keytab, if needed, by running the given block
+  def keytab_maybe_regen(path, new_path = nil, &block)
+    if Puppet::FileSystem.exist?(path)
+      unless Puppet::FileSystem.file?(path)
+        raise Puppet::ParseError, "Keytab exists but is not a file - '#{path}'"
       end
 
-      princ_keytab
+      return path
+    end
+
+    new_path ||= path
+    
+    # Puppet::Util::replace_file could be used, but it creates a file
+    # automatically and opens it.
+    # Unfortunately, kadmin can't seem to handle writing to empty files: it
+    # tries to read them first and breaks.
+    # So we just open a tempfile and close (unlinking it) right away instead,
+    # and just grab the path for our own use.
+
+    tmp_file = Puppet::FileSystem::Uniquefile.new(
+      Puppet::FileSystem.basename_string(new_path),
+      Puppet::FileSystem.dir_string(new_path))
+
+    tmp_path = tmp_file.path
+    tmp_file.close!
+
+    yield tmp_path
+
+    begin
+      File.rename(tmp_path, new_path)
+      Puppet::FileSystem.chmod(0600, new_path)
+    rescue SystemCallError => e
+      raise Puppet::ParseError, "Failed to store keytab: #{e}", e.backtrace
+    end
+
+    new_path
+  end
+
+  combined_keytab = File.join(keytab_dirs[:host], combined_id + '.keytab')
+
+  princs_max_mtime = princs.map(&:last_modified_time).max
+  keytab_maybe_regen(combined_keytab) do |combined_tmp|
+    # Check each principal individually and update the keytabs accordingly
+
+    princ_keytabs = princs.map do |princ|
+      clean_princ_name = princ.name.gsub(?/, ?$)
+      
+      princ_keytab_dir = File.join(keytab_dirs[:principals], clean_princ_name)
+      keytab_dir_maybe_create(princ_keytab_dir)
+
+      # We must increment the key version number of a possible newly generated
+      # keytab, since it will have a new key.
+      old_kt, new_kt = [princ.key_version, princ.key_version + 1].map do |kvno|
+        File.join(princ_keytab_dir,
+                  "#{princ.master_key_version}-#{kvno}.keytab")
+      end
+      
+      keytab_maybe_regen(old_kt, new_kt) do |tmp_kt|
+        begin
+          kadmin.keytab_add(princ.name, false, tmp_kt)
+        rescue Pupppet::Util::Krb5::KerberosError => e
+          raise Puppet::ParseError,
+            "Failed retrieving keytab for principal #{princ.name}: #{e}",
+            e.backtrace
+        end
+      end
     end
 
     # Generated the combined keytab from all the principal keytabs
-    ktutil = Puppet::Util::Krb5::Ktutil.new(combined_tmp_keytab)
-    princ_keytabs.each(&ktutil.method(:add_keytab))
-    ktutil.write()
-
-    keytab_replace(combined_tmp_keytab, combined_keytab, princs_max_mtime)
+    begin
+      ktutil = Puppet::Util::Krb5.ktutil_new(combined_tmp)
+      princ_keytabs.each(&ktutil.method(:add_keytab))
+      ktutil.write
+    rescue Puppet::Util::Krb5::KerberosError => e
+      raise Puppet::ParseError,
+        "Failed combining keytabs for host #{certname}: #{e}", e.backtrace
+    end
   end
 
-  IO.binread(combined_keytab)
+  begin
+    Puppet::FileSystem.binread(combined_keytab)
+  rescue SystemCallError => e
+    raise Puppet::ParseError,
+      "Failed reading combined keytab for host #{certname}: #{e}", e.backtrace
+  end
 end
